314d2e5563a93d9a9ae99f193c8a7edf2e454f02
Chris Zaikis <62755319+zaikis@users.noreply.github.com>
2023-07-15 15:55:25 +0300

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,46 +1,60 @@
 function Card() {
   return (
-    <div className="birthdayCard">
-      <div className="cardFront">
-        <div className="front-text">
-          <h3 className="happy">HAPPY</h3>
-          <h2 className="bday">BIRTHDAY</h2>
-          <h3 className="toyou">EVELINA!</h3>
-        </div>
-        <div className="wrap-deco">
-          <div className="decorations"></div>
-          <div className="decorationsTwo"></div>
-        </div>
-        <div className="wrap-decoTwo">
-          <div className="decorations"></div>
-          <div className="decorationsThree"></div>
-        </div>
-        <div className="plate">
-          <div className="cake"></div>
-          <div className="flame"></div>
-          <div className="flam"></div>
-        </div>
+    <div>
+      <div class="balloons">
+        <div class="balloon"></div>
+        <div class="balloon"></div>
+        <div class="balloon"></div>
+        <div class="balloon"></div>
+        <div class="balloon"></div>
+        <div class="balloon"></div>
+        <div class="balloon"></div>
+        <div class="balloon"></div>
+        <div class="balloon"></div>
       </div>
-
-      <div className="cardInside">
-        <div className="inside-text">
-          <h3 className="happy">HAPPY</h3>
-          <h2 className="bday">BIRTHDAY</h2>
-          <h3 className="toyou">EVELINA!</h3>
+      <div className="birthdayCard">
+        <div className="cardFront">
+          <div className="front-text">
+            <h3 className="happy">HAPPY</h3>
+            <h2 className="bday">BIRTHDAY</h2>
+            <h3 className="toyou">EVELINA!</h3>
+          </div>
+          <div className="wrap-deco">
+            <div className="decorations"></div>
+            <div className="decorationsTwo"></div>
+          </div>
+          <div className="wrap-decoTwo">
+            <div className="decorations"></div>
+            <div className="decorationsThree"></div>
+          </div>
+          <div className="plate">
+            <div className="cake"></div>
+            <div className="flame"></div>
+            <div className="flam"></div>
+          </div>
         </div>
-        <div className="wishes">
-          <p>
-            {" "}
-            Σας Προσκαλώ στο παρτυ των γενεθλίων μου το Σάββατο 17 Δεκεμβρίου
-            2023 και ώρα 6μ.μ απένατι από το σπιτι μου στην Pizza Πακέτο.
-          </p>
-          <p className="name">EVELINA</p>
+
+        <div className="cardInside">
+          <div className="inside-text">
+            <h3 className="happy">HAPPY</h3>
+            <h2 className="bday">BIRTHDAY</h2>
+            <h3 className="toyou">EVELINA!</h3>
+          </div>
+          <div className="wishes">
+            <p>
+              {" "}
+              Σας Προσκαλώ στο παρτυ των γενεθλίων μου το Σάββατο 17 Δεκεμβρίου
+              2023 και ώρα 6μ.μ απένατι από το σπιτι μου στην Pizza Πακέτο.
+            </p>
+            <p className="name">EVELINA</p>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 export default Card;
+
 
 
 
