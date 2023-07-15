bc699a8b0124ab581e3316c498e93fb78bd6767f
Chris Zaikis <62755319+zaikis@users.noreply.github.com>
2023-07-15 14:04:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import Card from "./Card";
 
 import "./styles.css";
 import Confetti from "react-confetti";
+
 export default function App() {
   return (
     <div>
-      <Confetti
-        size={8}
-        shape="circle"
-        colors={["#f44336", "#9c27b0", "#3f51b5"]}
-        wind={0}
-        gravity={0.2}
-      />
+      <Confetti />
       <Card />
     </div>
   );
 }
+
 
