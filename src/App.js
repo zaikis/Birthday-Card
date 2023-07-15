7b11d776df68add9dfc6fcea815be238ac86d4af
Chris Zaikis <62755319+zaikis@users.noreply.github.com>
2023-07-15 14:40:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Card from "./Card";
-
+import Balloons from "./Balloons";
 import "./styles.css";
 import Confetti from "react-confetti";
 
 export default function App() {
   return (
     <div>
+      <Balloons />
       <Confetti />
       <Card />
     </div>
   );
 }
-
 
