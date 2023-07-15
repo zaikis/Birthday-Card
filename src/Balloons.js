e53beae226a891df98f533e4f03f257a583732fe
Chris Zaikis <62755319+zaikis@users.noreply.github.com>
2023-07-15 14:40:31 +0300

    Add files via upload

---
+++ src/Balloons.js
@@ -0,0 +1,16 @@
+function Balloons() {
+  return (
+    <div class="balloons">
+      <div class="balloon"></div>
+      <div class="balloon"></div>
+      <div class="balloon"></div>
+      <div class="balloon"></div>
+      <div class="balloon"></div>
+      <div class="balloon"></div>
+      <div class="balloon"></div>
+      <div class="balloon"></div>
+      <div class="balloon"></div>
+    </div>
+  );
+}
+export default Balloons;
